5829a9b126b34140a2fde49fd2b0f25ebdaa3e5b
Joshua Fung <joshua.fung@alum.utoront.ca>
2021-10-14 17:33:09 -0400

    added stiffmat.py

---
+++ stiffmat.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 14 17:13:43 2021
+
+Laminate stiffness matrix calculator 
+
+Calculate the stiffness matrix of a given ply 
+
+@author: Joshua Fung
+"""
+import numpy as np
+
+class Laminate:
+    def __init__(self):
+        pass
+    
+class ply:
+    def __init__(self):
+        pass
+
+
+    
+
