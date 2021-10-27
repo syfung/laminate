39ea846b30e4c8d75a92b870d2bc3c1faa642bea
syfung <joshua.fung@alum.utoronto.ca>
2021-10-27 19:09:58 -0400

    strain function

--- deform.py
+++ deform.py
@@ -8,3 +8,14 @@ import numpy as np
 
 def deformation(abd_inv, load):
     return abd_inv.dot(load)
+
+def strain(laminate, mid_plane_deformation):
+    mid_ply_z = []
+    for k in range(len(laminate.z_s)):
+        mid_ply_z.append((laminate.z_s[1] - laminate.z_s[0])/2)
+    ply_deformation = np.empty([0,3])
+    for z in mid_ply_z:
+        ply_deformation = np.append(ply_deformation,
+                                    [mid_plane_deformation[0:3] + z * mid_plane_deformation[3:6]],
+                                    axis=0)
+    return ply_deformation
