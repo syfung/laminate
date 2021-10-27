adb0ae59235cffc0c003cfb4d633af740fb5111c
syfung <joshua.fung@alum.utoronto.ca>
2021-10-27 17:24:33 -0400

    added deform.py

---
+++ deform.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 27 17:22:11 2021
+
+@author: Joshua Fung
+"""
+import numpy as np
+
+def deformation(abd_inv, load):
+    return lam.abd_inv().dot(load)
